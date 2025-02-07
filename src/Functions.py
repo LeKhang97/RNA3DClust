35e9c312a8c94106fa00a4ee4985009ec58b5e9d
Le Quoc Khang <70560694+LeKhang97@users.noreply.github.com>
2025-02-07 15:56:37 +0100

    Add files via upload

--- src/Functions.py
+++ src/Functions.py
@@ -263,133 +263,17 @@ def check_C(result, threshold):
 
         return data, [i for i in result[-1] if len(i) >= threshold], removed_chain_index
 
-    
-def domain_overlap_matrix(lists_label, list_residue = None): #Order in lists_label: ground_truth, prediction 
-    if list_residue == None:
-        list_residue = range(len(lists_label[0]))
-    
-    group_label = {'pred': lists_label[1], 'true': lists_label[0]}
-
-    group_residue = {'pred': {}, 'true': {}}
-
-    for key in group_label.keys():
-        for label in set(group_label[key]):
-            group_residue[key][label] = [list_residue[i] for i in range(len(list_residue)) if group_label[key][i] == label]
-
-    domain_matrix = []
-    # Compare if pred has more cluster than ref:
-    comp_pred_ref = len(set(group_label['pred'])) - len(set(group_label['true']))
-    for label in sorted(set(group_label['pred'])): 
-        row = []
-        for label2 in sorted(set(group_label['true'])):
-            row += [len([intersect for intersect in group_residue['pred'][label] if intersect in group_residue['true'][label2]])]
-        if comp_pred_ref > 0:
-            row += [0]*(comp_pred_ref)
-        domain_matrix += [row]
-    
-    if comp_pred_ref < 0:
-        for i in range(-comp_pred_ref):
-            domain_matrix += [[0]*len(set(group_label['true']))]
-    
-    min_labels = [min(set(group_label['pred'])), min(set(group_label['true']))]
-    #return domain_matrix
-    return domain_matrix, min_labels
-
-def NDO(domain_matrix, len_rnas, min_labels = [0,0]):
-    #print(domain_matrix, [min_labels[0]**2, min_labels[1]**2])
-    domain_matrix_no_linker = [row[(min_labels[1] == -1):] for row in domain_matrix[(min_labels[0] == -1):]]
-    domain_matrix_no_linker = np.asarray(domain_matrix_no_linker)
-    domain_matrix = np.asarray(domain_matrix)
-    
-    sum_col = np.sum(domain_matrix, axis = 0)
-    
-    sum_row =  np.sum(domain_matrix, axis = 1)
-    
-    max_col = np.amax(domain_matrix_no_linker, axis = 0)
-    
-    max_row = np.amax(domain_matrix_no_linker, axis = 1)
-    
-    Y = 0
-    #print(domain_matrix,domain_matrix_no_linker, sum_col, sum_row, max_col, max_row, sep='\n')
-    for row in range(domain_matrix_no_linker.shape[0]):
-
-        Y += 2*max_row[row] - sum_row[row+(min_labels[0] == -1)]
-        
-    for col in range(domain_matrix_no_linker.shape[1]):
-        Y += 2*max_col[col] - sum_col[col+(min_labels[1] == -1)]
-
-    score = Y/(2*(len_rnas - sum_col[0]*(min_labels[1] == -1)))
-    
-    return score
-
-def domain_boundary(lists_label, list_residue = None): #Order in lists_label: ground_truth, prediction 
-        if list_residue == None:
-            list_residue = range(len(lists_label[0]))
-        
-        group_label = {'pred': lists_label[1], 'true': lists_label[0]}
-
-        group_residue = {'pred': {}, 'true': {}}
-        
-        group_boundary = {'pred': {}, 'true': {}}
-        
-        for key in group_label.keys():
-            list_boundary = []
-            for label in set(group_label[key]):
-                #group_residue[key][label] = list_to_range([lists_residue[key][i] for i in range(len(lists_residue[key])) if lists_label[key][i] == label])
-                group_residue[key][label] = [ list_to_range(list_residue[i] for i in range(len(list_residue)) if group_label[key][i] == label)]
-            
-            
-                list_boundary += flatten([[(j[0],j[-1]) for j in i] for i in group_residue[key][label]])
-            
-            group_boundary[key] = list_boundary
-            
-        return group_boundary
-
-
-'''def DBD(lists_label, list_residue = None, threshold = 8):
-    group_boundary = domain_boundary(lists_label, list_residue)
-    
-    num_domains = max(len(group_boundary[key]) for key in group_boundary.keys()) - 1
-    
-    dict_boundary = {}
-    for key in group_boundary.keys():
-        dict_boundary[key] = [ele[0] for ele in group_boundary[key]]
-    
-    score = 0
-    #for i,j in itertools.product(dict_boundary['pred'], dict_boundary['true']):
-    if len(dict_boundary['pred']) >= len(dict_boundary['true']):
-        key1 = 'pred'; key2 = 'true'
-    else:
-        key2 = 'pred'; key1 = 'true'
-    
-    print(dict_boundary)
-    for i in dict_boundary[key1]:
-        max_bound_dist = 0
-        for j in dict_boundary[key2]:
-            if threshold - abs(i - j) >= max_bound_dist:
-                max_bound_dist = threshold - abs(i - j)
-                t = j
-            
-        if i != 0 and t != 0 and max_bound_dist != 100000:
-            score += max_bound_dist
-            print(max_bound_dist, i, t)
-    
-    score = score/(threshold*num_domains)
-    
-    print(num_domains, group_boundary)
-    return score'''
-
 def post_process(cluster_list, res_list = False):
     cluster_list2 = cluster_list.copy()
     if res_list == False:
         res_list = list(range(len(cluster_list2)))
     
-    for t in range(6):
+    n = 10 # Number of iterations
+    for t in range(n):
         #ranges contain clusters with ranges, values contain clusters with values
         min_cluster = (min(set(cluster_list2)))**2
         list_of_ranges = [list_to_range([v for p,v in enumerate(res_list) if cluster_list2[p] == label]) for label in sorted(set(cluster_list2))]
         list_of_values = [[v for p,v in enumerate(res_list) if cluster_list2[p] == label] for label in sorted(set(cluster_list2))]
-        
         pos_ranges = 0
         
         pos_to_change = []
@@ -400,7 +284,7 @@ def post_process(cluster_list, res_list = False):
                     #if len(subranges) < 100:
                     if True:
                         # If the residues right before the selected segment exists, label the segment and cluster that contains it
-                        for t1 in range(1,6):
+                        for t1 in range(1,11):
                             if subranges[0] - t1 in values:
                                 c1 = list_of_values.index(values) 
                                 l1 = [l for l in list_of_ranges[c1] if subranges[0] - t1 in l][0]
@@ -408,13 +292,12 @@ def post_process(cluster_list, res_list = False):
                                 break
 
                         # If the residues right after the selected segment exists, label the segment and cluster that contains it
-                        for t2 in range(1,6):
+                        for t2 in range(1,7):
                             if subranges[-1] + t2 in values:
                                 c2 = list_of_values.index(values)
                                 l2 = [l for l in list_of_ranges[c2] if subranges[-1] + t2 in l][0]
                                 label2 = True
                                 break
-
                 # If the selected segment is an outliner segment
                 if min_cluster == 1 and pos_ranges == 0:
                     # If 2 segments on both sides have the same label, label the outliner that label if it's < 30
@@ -489,106 +372,13 @@ def post_process(cluster_list, res_list = False):
                         if len(l2) > len(subranges) and len(subranges) < 30 and len(l2) >= 30:
                             val = list(sorted(set(cluster_list2)))[c2]
                             pos_to_change += flatten([[(j, val) for j in range(len(res_list)) if res_list[j] == i] for i in subranges])
-
-            pos_ranges += 1
         
+            pos_ranges += 1
+
         for pos,val in pos_to_change:
             cluster_list2[pos] = val
     
     return cluster_list2    
-
-def merge_ele_matrix(mtx, list_range_true, list_range_pred):
-    merged_mtx_row = []
-    for row in mtx:
-        new_row = row.copy()  # Create a copy of the row to avoid modifying the original row
-        for label_pos in range(len(list_range_true)):
-            s = label_pos
-            e = s + len(list_range_true[label_pos])
-            new_row = new_row[:s] + [sum(new_row[s:e])] + new_row[e:]
-        merged_mtx_row.append(new_row)
-    
-    merged_mtx_row = np.array(merged_mtx_row).T.tolist()
-    
-    merged_mtx = []
-    for row in merged_mtx_row:
-        new_row = row.copy()  # Create a copy of the row to avoid modifying the original row
-        for label_pos in range(len(list_range_pred)):
-            s = label_pos
-            e = s + len(list_range_pred[label_pos])
-            new_row = new_row[:s] + [sum(new_row[s:e])] + new_row[e:]
-        merged_mtx.append(new_row)
-
-        
-    merged_mtx = np.array(merged_mtx).T  # Transpose to get the correct shape
-    
-    return merged_mtx
-
-def domain_distance(segment1, segment2):
-    d = abs(min(segment1) - min(segment2))
-    d += abs(max(segment1) - max(segment2))
-    
-    return d/2
-
-def domain_distance_matrix(lists_label, list_residue = None):#Order in lists_label: ground_truth, prediction 
-    if list_residue == None:
-        list_residue = range(len(lists_label[0]))
-    
-    group_label = {'pred': lists_label[1], 'true': lists_label[0]}
-
-    group_residue = {'pred': {}, 'true': {}}
-    for key in group_label.keys():
-        for label in set(group_label[key]):
-            group_residue[key][label] = [list_residue[i] for i in range(len(list_residue)) if group_label[key][i] == label]
-    
-    domain_distance_mtx = []
-    for label1 in sorted(set(group_label['pred'])):
-        for segment1 in list_to_range(group_residue['pred'][label1]):
-            row = []
-            for label2 in sorted(set(group_label['true'])):
-                for segment2 in list_to_range(group_residue['true'][label2]):
-                    #print(segment1, segment2)
-                    x = domain_distance(segment2, segment1)
-                    row += [x]
-                    #print(x, end = '\n\n')
-            
-            domain_distance_mtx += [row]
-    
-    lst_to_range_true = [list_to_range(group_residue['true'][label1]) for label1 in sorted(set(group_label['true']))]
-    lst_to_range_pred = [list_to_range(group_residue['pred'][label1]) for label1 in sorted(set(group_label['pred']))]
-    
-    return domain_distance_mtx, lst_to_range_true, lst_to_range_pred
-
-def DBD(domain_distance_mtx, list_range_true, list_range_pred, threshold = 50):
-    scoring_mtx = []
-    for row in domain_distance_mtx:
-        scoring_mtx += [[threshold - i if i < threshold else 0 for i in row]]
-    
-    merged_scoring_mtx = merge_ele_matrix(scoring_mtx, list_range_true, list_range_pred)
-
-    merged_scoring_mtx = np.asarray(merged_scoring_mtx)
-    scoring_mtx = np.asarray(scoring_mtx)
-    #print(merged_scoring_mtx, scoring_mtx, sep = '\n')
-    if scoring_mtx.shape[0] >= scoring_mtx.shape[1]:
-        max_row = np.amax(merged_scoring_mtx, axis = 1)
-        total_score = sum(max_row)/(threshold*scoring_mtx.shape[0])
-    else:
-        max_col = np.amax(merged_scoring_mtx, axis = 0)
-        total_score = sum(max_col)
-        total_score = sum(max_col)/(threshold*scoring_mtx.shape[1])
-    
-    return total_score
-
-def DCS(truth,pred, outliner = False):
-    if outliner == False:
-        truth = [i for i in truth if i != -1]
-        pred =  [i for i in pred if i != -1]
-    
-    count_truth = len(set(truth))
-    count_pred = len(set(pred))
-    
-    DCS = (max((count_truth), count_pred) - abs(count_truth - count_pred))/max(count_truth, count_pred)
-    
-    return DCS
 
 def inf(tup_of_tups, val):
     start_eles = [min(tup) for tup in tup_of_tups if min(tup) <= val]
@@ -621,66 +411,6 @@ def find_tuple_index(tuples, element, order = False):
                     if element == t[-1]:
                         return i
     return -1  # Return -1 if the element is not found in any tuple
-
-'''def top_down_algo(tup_of_tups, pos_list):
-    tup_of_tups = tuple(tup_of_tups)
-    pos_list = sorted(pos_list)
-    #l = [largest_smaller_than(acc_sum_list, pos) for pos in pos_list]
-    new_tups = []
-    old_tup_of_tups = tup_of_tups
-    #print(tup_of_tups)
-    for ele in pos_list:
-        len_list = [tup[-1] - tup[0] for tup in tup_of_tups]
-        acc_sum_list = list(itertools.accumulate(len_list))
-        
-        inf_start, inf_end = inf(tup_of_tups, ele)
-        sup_start, sup_end = sup(tup_of_tups, ele)
-        X = [i for i in range(len(acc_sum_list)) if acc_sum_list[i] < ele]
-        #ind_inf_end = find_tuple_index(tup_of_tups, inf_end)
-        ind_inf_end = max(X) if bool(X) else -1
-        #print(acc_sum_list, ele,ind_inf_end)
-
-        if ind_inf_end == -1:
-            x = (tup_of_tups[ind_inf_end+1][0], tup_of_tups[ind_inf_end+1][0] + ele - 0)
-            y = (tup_of_tups[ind_inf_end+1][0] + ele - 0, sup_end)
-            tup_of_tups = tuple( tuple([x,y]) + tup_of_tups[1:])
-        else:
-
-            x = (tup_of_tups[ind_inf_end+1][0], tup_of_tups[ind_inf_end+1][0] + ele - acc_sum_list[ind_inf_end]) # need to fix
-            y = (tup_of_tups[ind_inf_end+1][0] + ele - acc_sum_list[ind_inf_end], tup_of_tups[ind_inf_end+1][1]) # need to fix
-            tup_of_tups = tuple(tup_of_tups[:ind_inf_end+1] +  tuple([x,y]) + tup_of_tups[ind_inf_end + 2:])
-        
-        #print('x,y:', x,y)
-        
-        #print(tup_of_tups)
-        tup_of_tups = tuple([i for i in tup_of_tups if i[0] != i[-1]])
-        
-    new_tups = tuple(i for i in tup_of_tups if i not in old_tup_of_tups)
-
-    print(new_tups)
-    if len(new_tups) == 2:
-        tups1 = tup_of_tups[:tup_of_tups.index(new_tups[0])+1]
-        tups2 = tup_of_tups[tup_of_tups.index(new_tups[1]):]
-    
-    elif len(new_tups) == 3:
-        tups1 = tup_of_tups[:tup_of_tups.index(new_tups[0])+1] + tup_of_tups[tup_of_tups.index(new_tups[2]):]
-        tups2 = tup_of_tups[tup_of_tups.index(new_tups[1]):tup_of_tups.index(new_tups[2])]
-        
-    elif len(new_tups) == 4:
-        tups1 = tup_of_tups[:tup_of_tups.index(new_tups[0])+1] + tup_of_tups[tup_of_tups.index(new_tups[3]):]
-        tups2 = tup_of_tups[tup_of_tups.index(new_tups[1]):tup_of_tups.index(new_tups[2])+1]
-    
-    elif len(new_tups) == 1:
-        tups1 = tup_of_tups[:tup_of_tups.index(new_tups[0])]
-        tups2 = tup_of_tups[tup_of_tups.index(new_tups[0])+1:]
-    
-    else:
-        tups1 = tup_of_tups[:ind_inf_end+2]
-        tups2 = tup_of_tups[ind_inf_end+2:]
-
-    return tups1,tups2'''
-
-import itertools
 
 def top_down_algo(tup_of_tups, pos_list):
     # Convert tuples to lists for faster updates
@@ -839,56 +569,6 @@ def tup_pos_process(tup_of_tup):
     
     return result
 
-'''def contact_map_algo(original_frags):
-    frags = [original_frags.copy()]
-    frag_ind= [tuple([(0,len(original_frags))])]
-    s = 0
-    while True:
-        flag = 0
-        new_frags = []
-        new_frag_ind = []
-        for frag,ind in zip(frags,frag_ind):
-            DIS2 = {}
-            for position1 in range(30,len(frag)-30):
-                for position2 in range(position1+30,len(frag)-30):
-                    if position2 == None:
-                        continue
-                    d = np.linalg.norm(np.array(frag[position1]) - np.array(frag[position2]))
-                    if d < 14:
-                        DIS2[position1, position2] = DISinter(np.concatenate((frag[:position1], frag[position2:]), axis=0), frag[position1:position2])
-                    for position in [position1, position2]:
-                        if (position,) not in DIS2.keys():
-                            DIS2[position,] = DISinter(frag[:position], frag[position:])             
-
-            if bool(DIS2.keys()) == False:
-                new_frag_ind += tuple([ind])
-                new_frags += [frag]
-                continue
-
-            min_key = min(DIS2, key=DIS2.get)
-            min_value = DIS2[min_key].copy()
-
-            if min_value < DISintra(frag)/2:
-                print('index and key: ', ind, min_key)
-                x = top_down_algo(ind, min_key)
-                print(x)
-                new_frag_ind += x
-                flag = 1
-                new_frags += [np.concatenate([original_frags[i[0]:i[1]] for i in m], axis = 0) for m in x]
-                
-            else:
-                new_frag_ind += tuple([ind])
-                new_frags += [frag]
-
-        frag_ind = new_frag_ind
-        frags = new_frags
-
-        #print(frag_ind)
-        s += 1
-        if flag == 0:
-            result = bot_up_algo(frags, frag_ind)
-            return result'''
-
 def contact_map_algo(original_frags):
     frags = [original_frags.copy()]
     frag_ind = [tuple([(0, len(original_frags))])]
@@ -991,9 +671,10 @@ def split_pdb_by_clusters(pdb_file, clusters, output_prefix, chain=None):
     
     return cluster_lines
 
+
 def extend_missing_res(list_label, list_residue):
-    max_res = max(list_residue)
-    list_residue_ext = list(range(1, max_res + 1))
+    max_res = max(list_residue); min_res = min(list_residue)
+    list_residue_ext = list(range(min_res, max_res + 1))
 
     # Use a dictionary for fast lookups
     residue_to_label = dict(zip(list_residue, list_label))
